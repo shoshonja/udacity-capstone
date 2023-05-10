f85ff491db311289f6f98175f03c0395d67cf069
ivansosic <ivan.sosic@greyp.com>
2023-05-10 08:16:12 +0200

    first commit

---
+++ app/src/main/java/com/bikes/greyp/udacitycapstoneproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bikes.greyp.udacitycapstoneproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
